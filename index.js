d94e3d43a1fd728fbac02a0068cb2f5f788d4198
Anuja Manoharan <anujamanoharan@gmail.com>
2017-11-20 23:33:30 +0530

    settle up

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function addBill(){
 	$("#addBill").css("display", "block");
 	$("#main").css("opacity", "0.2");
 }
+
+function settleUp(){
+	$("#settleUp").css("display", "block");
+	$("#main").css("opacity", "0.2");
+}
