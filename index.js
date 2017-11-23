9ddd4d36a2e90567aaaba932af397a5f304ca213
Anuja Manoharan <anujamanoharan@gmail.com>
2017-11-23 22:03:06 +0530

    add cancel

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function settleUp(){
 	$("#settleUp").css("display", "block");
 	$("#main").css("opacity", "0.2");
 }
+
+function hide(obj){
+	$(obj).css("display", "none");
+	$("#main").css("opacity", "1");
+}
